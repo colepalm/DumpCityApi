50bb22697f5bcb84f827f990b8426a7f0e7c67c7
colepalm <palm.cole@gmail.com>
2019-06-16 16:02:11 -0600

    add messages to schema

--- src/server.js
+++ src/server.js
@@ -10,10 +10,25 @@ let users = {
   1: {
     id: '1',
     username: 'Robin Wieruch',
+    messageIds: [1],
   },
   2: {
     id: '2',
     username: 'Dave Davids',
+    messageIds: [2],
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
   },
 };
 
@@ -24,11 +39,21 @@ const schema = gql`
     users: [User!]
     me: User
     user(id: ID!): User
+
+    messages: [Message!]!
+    message(id: ID!): Message!
   }
 
   type User {
     id: ID!
     username: String!
+    messages: [Message!]
+  }
+
+  type Message {
+      id: ID!
+      text: String!
+      user: User!
   }
 `;
 
@@ -36,7 +61,17 @@ const resolvers = {
   Query: {
     users: () => Object.values(users),
     user: (parent, { id }) => users[id],
-    me: (parent, args, { me }) => me
+    me: (parent, args, { me }) => me,
+    messages: () => Object.values(messages),
+    message: (parent, { id }) => messages[id]
+  },
+
+  User: {
+    messages: user => Object.values(messages).filter(message => message.userId === user.id)
+  },
+
+  Message: {
+    user: message => users[message.userId]
   }
 };
 
