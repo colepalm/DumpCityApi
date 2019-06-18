e3922b47d0f377577ad15cca3a8c2a23553c88be
colepalm <palm.cole@gmail.com>
2019-06-17 22:04:13 -0600

    first mutation

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
+import uuidv4 from 'uuid'
 
 const app = express();
 
@@ -44,6 +45,11 @@ const schema = gql`
     message(id: ID!): Message!
   }
 
+  type Mutation {
+      createMessage(text: String!): Message!
+      deleteMessage(id: ID!): Boolean!
+  }
+
   type User {
     id: ID!
     username: String!
@@ -64,6 +70,34 @@ const resolvers = {
     me: (parent, args, { me }) => me,
     messages: () => Object.values(messages),
     message: (parent, { id }) => messages[id]
+  },
+
+  Mutation: {
+    createMessage: (parent, { text }, { me }) => {
+      const id = uuidv4();
+      const message =  {
+        id,
+        text,
+        userId: me
+      };
+
+      messages[id] = message;
+      users[me.id].messageIds.push(id);
+
+      return message;
+    },
+
+    deleteMessage: (parent, { id }) => {
+      const { [id]: message, ...otherMessages } = messages;
+
+      if (!message) {
+        return false;
+      }
+
+      messages = otherMessages;
+
+      return true;
+    },
   },
 
   User: {
