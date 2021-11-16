c37c5b862531c1c69b44dbdd6113bde74deb134a
colepalm <palm.cole@gmail.com>
2021-11-15 23:41:42 -0700

    note

--- hack/populate-shows.ts
+++ hack/populate-shows.ts
@@ -9,7 +9,7 @@ const pt = new PtService();
 const dumpCity = new DumpCityService();
 
 const main = async () => {
-    let index = 27;
+    let index = 19; // start here next time
     while(index > 0) {
         const response = await pt.client.get(`/bands/9/shows?pageSize=100&page=${index}`)
 
