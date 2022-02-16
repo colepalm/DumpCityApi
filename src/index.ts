6ed43b5b845fbf00900ede2819c0e51b4e5661a7
colepalm <palm.cole@gmail.com>
2022-02-15 21:11:29 -0700

    move to migrated db

--- src/index.ts
+++ src/index.ts
@@ -13,17 +13,6 @@ import {
 } from './resolvers';
 
 export async function createApolloServer(): Promise<ApolloServer> {
-    // const connectionOptions: PostgresConnectionOptions = {
-    //     name: `default`,
-    //     type: `postgres`,
-    //     ssl: true,
-    //     url: 'postgres://cigyexiijubsze:5d48077063d92d2c7d90a2babfda86e1c34260a95783827b8242d8f836e9c3c8@ec2-44-194-54-186.compute-1.amazonaws.com:5432/dbo89ttmshhh7q',
-    //     extra: {
-    //         ssl: {
-    //             rejectUnauthorized: false,
-    //         },
-    //     }
-    // };
 
     const connection = await createConnection();
     connection.synchronize();
