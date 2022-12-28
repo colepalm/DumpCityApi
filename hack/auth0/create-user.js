cd5dd09a4c87059dbb4149a4fc5d34436a6c4ef5
colepalm <palm.cole@gmail.com>
2022-12-28 13:40:58 -0600

    metadata addition to user model working

--- hack/auth0/create-user.js
+++ hack/auth0/create-user.js
@@ -1,13 +1,15 @@
 const axios = require('axios');
 const graphql = require('graphql')
 const gql = require('graphql-tag');
+
 /**
- * Handler that will be called during the execution of a PostUserRegistration flow.
+ * Handler that will be called during the execution of a PreUserRegistration flow.
  *
- * @param {Event} event - Details about the context and user that has registered.
+ * @param {Event} event - Details about the context and user that is attempting to register.
+ * @param {PreUserRegistrationAPI} api - Interface whose methods can be used to change the behavior of the signup.
  */
-exports.onExecutePostUserRegistration = async (event) => {
-    const createSetMutation = gql`
+exports.onExecutePreUserRegistration = async (event, api) => {
+    const createUserMutation = gql`
         mutation createUser($email:String!){
             createUser(user: {
                 email: $email
@@ -15,12 +17,16 @@ exports.onExecutePostUserRegistration = async (event) => {
         }
     `;
 
-    await axios.post("https://dump-city-api.herokuapp.com/graphql", {
-        query: graphql.print(createSetMutation),
+    await axios.post("https://dump-city-api.fly.dev/graphql", {
+        query: graphql.print(createUserMutation),
         variables: {
             email: event.user.email
         }
     })
-        .then(res => console.log(res))
+        .then(res => {
+            if (res.data.data.createUser.id) {
+                api.user.setAppMetadata("id", res.data.data.createUser.id);
+            }
+        })
         .catch(err => console.log(err))
 };
--- src/resolvers/user/UserResolver.ts
+++ src/resolvers/user/UserResolver.ts
@@ -1,13 +1,19 @@
 import { Arg, Mutation, Resolver } from 'type-graphql';
+import { InjectRepository } from 'typeorm-typedi-extensions';
+import { Repository } from 'typeorm';
 
 import { User } from '../../models/user/User';
 import { CreateUserInput } from '../../inputs/user/CreateUserInput';
 
 @Resolver()
 export class UserResolver {
+    constructor(
+        @InjectRepository(User) private readonly userRepository: Repository<User>
+    ) { }
+
     @Mutation(() => User)
     async createUser(@Arg('user') userInput: CreateUserInput) {
-         const user = User.create({
+         const user = this.userRepository.create({
              email: userInput.email,
              username: userInput.email
          });
